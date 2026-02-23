019eee8541bb6d00cd298eb3b96014ca15e89073
holkexyz <holke.brammer@gmail.com>
2026-02-23 21:06:38 +0100

    Fix ESLint ignores, add type:module, add image remotePatterns (do9.2, do9.4, do9.12)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hypercerts.org",
+      },
+      {
+        protocol: "https",
+        hostname: "app.hypercerts.org",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
